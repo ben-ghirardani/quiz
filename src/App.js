2482059d99a1cff729dc8521177a54359b739ed0
Ben <pretty.fiendish@gmail.com>
2020-05-09 15:27:03 +0100

    added fetch for category data

--- src/App.js
+++ src/App.js
@@ -6,18 +6,33 @@ class App extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        currentQuiz: null
+        categories: null
       }
-  }  
-
-    render() {
-      return(
-        <div className="App">
-          <p>Quiz!</p>
-        </div>
-      )
-    }
-
+      this.getCategories = this.getCategories.bind(this);
   }
+
+  componentDidMount() {
+    this.getCategories()
+  };
+
+  getCategories() {
+    fetch('https://opentdb.com/api_category.php')
+      .then(response => response.json())
+      .then(data => this.setState({categories: data}))
+      .catch(error => console.log(error.message));
+  };
+
+// when randomizing the array of answers use the following Fisher-Yates (aka Knuth) Shuffle -
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+  render() {
+    return(
+      <div className="App">
+        <p>Quiz!</p>
+      </div>
+    )
+  }
+
+}
 
 export default App;
