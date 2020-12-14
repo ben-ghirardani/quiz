c980df755c53b2107ee645db0e46df7b4291e6c3
Ben <pretty.fiendish@gmail.com>
2020-12-14 15:56:13 +0000

    begun adding framer-motion animations to components

--- src/components/landing_page/LandingPage.js
+++ src/components/landing_page/LandingPage.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import './LandingPage.css';
+import {motion} from "framer-motion";
 
 export default function landingPage(props) {
-  return <div className="landing-page" onClick={props.clickLandingPage} >Quiz!</div>
+  return <motion.div 
+    className="landing-page" 
+    onClick={props.clickLandingPage}
+    initial={{scale: 0}}
+    animate={{scale: 1}}
+    transition={{
+      delay: 1,
+      type: "spring",
+      stiffness: 260,
+      damping: 20
+    }}
+  >
+  Quiz!
+  </motion.div>
 }
